04f2c1c20ad8b549614fab2b86758c2896909095
Yohai <noizzhack7@gmail.com>
2025-12-08 10:11:07 +0200

    Update job mock data with new positions and enhanced descriptions; modify front-end styles for improved color scheme and add routing for the new development plan component.

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { QuestionnaireComponent } from './questionnaire/questionnaire.component';
 import { ProfileComponent } from './profile/profile.component';
+import { DevPlanComponent } from './dev-plan/dev-plan.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'profile', component: ProfileComponent },
+  { path: 'dev-plan', component: DevPlanComponent },
   { path: 'questionnaire', component: QuestionnaireComponent, data: { testMode: false } },
   { path: 'questionnaire/test', component: QuestionnaireComponent, data: { testMode: true } },
   { path: '**', redirectTo: '' }
